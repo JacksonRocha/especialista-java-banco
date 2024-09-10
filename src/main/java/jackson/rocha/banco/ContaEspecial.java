257d1a220607759d063a84ead0ad2fb192173c66
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-10 20:40:53 -0300

    Projeto para trabalhar com herança

---
+++ src/main/java/jackson/rocha/banco/ContaEspecial.java
@@ -0,0 +1,101 @@
+package jackson.rocha.banco;
+
+public class ContaEspecial {
+
+    private Titular titular;
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    private double valorTotalRendimento;
+    private double tarifaMensal;
+    private double limiteChequeEspecial;
+
+    public Titular getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Titular titular) {
+        this.titular = titular;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double getSaldoDisponivel() {
+        return getSaldo() + getLimiteChequeEspecial();
+    }
+
+    public double getValorTotalRendimento() {
+        return valorTotalRendimento;
+    }
+
+    public double getTarifaMensal() {
+        return tarifaMensal;
+    }
+
+    public void setTarifaMensal(double tarifaMensal) {
+        this.tarifaMensal = tarifaMensal;
+    }
+
+    public double getLimiteChequeEspecial() {
+        return limiteChequeEspecial;
+    }
+
+    public void setLimiteChequeEspecial(double limiteChequeEspecial) {
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+
+    public void creditarRendimento(double porcentualJuros) {
+        double valorRendimentos = getSaldo() * porcentualJuros / 100;
+        this.valorTotalRendimento += valorRendimentos;
+        depositar(valorRendimentos);
+    }
+
+    public void sacar(double valorSaque) {
+        if (valorSaque <= 0) {
+            throw new IllegalArgumentException("Valor do saque deve ser maior que zero");
+        }
+        if (getSaldoDisponivel() < valorSaque) {
+            throw new RuntimeException("Saldo insuficiente para saque");
+        }
+        saldo -= valorSaque;
+    }
+
+    public void depositar(double valorDeposito) {
+        if (valorDeposito <= 0) {
+            throw new IllegalArgumentException("Valor do deposito deve ser maior que zero");
+        }
+        saldo += valorDeposito;
+    }
+
+    public void imprimirDemonstrativo() {
+        System.out.println();
+        System.out.printf("Agência: %d%n", getAgencia());
+        System.out.printf("Conta: %d%n", getNumero());
+        System.out.printf("Titular: %s%n", getTitular().getNome());
+        System.out.printf("Saldo: %.2f%n", getSaldo());
+        System.out.printf("Saldo disponível: %.2f%n", getSaldoDisponivel());
+    }
+
+    public void debitarTarifaMensal() {
+        sacar(getTarifaMensal());
+    }
+}
