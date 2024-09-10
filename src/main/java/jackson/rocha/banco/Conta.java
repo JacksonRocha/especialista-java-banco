bf8d965b4d7787e7f3e613c338745dc143801434
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-10 20:37:01 -0300

    Projeto para trabalhar com herança

--- src/main/java/jackson/rocha/banco/Conta.java
+++ src/main/java/jackson/rocha/banco/Conta.java
@@ -2,22 +2,10 @@ package jackson.rocha.banco;
 
 public class Conta {
 
-    public static final int NORMAL = 0;
-    public static final int INVESTIMENTO = 1;
-    public static final int ESPECIAL = 2;
-
     private Titular titular;
     private int agencia;
     private int numero;
     private double saldo;
-    private int tipo = NORMAL;
-
-    // conta investimento
-    private double valorTotalRendimento;
-
-    // conta especial
-    private double tarifaMensal;
-    private double limiteChequeEspecial;
 
     public Titular getTitular() {
         return titular;
@@ -47,63 +35,11 @@ public class Conta {
         return saldo;
     }
 
-    public double getSaldoDisponivel() {
-        return getSaldo() + getLimiteChequeEspecial();
-    }
-
-    public int getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(int tipo) {
-        if (tipo != NORMAL && tipo != INVESTIMENTO && tipo != ESPECIAL) {
-            throw new IllegalArgumentException("Tipo inválido" + tipo);
-        }
-        this.tipo = tipo;
-
-        if (tipo != ESPECIAL) {
-            this.limiteChequeEspecial = 0;
-        }
-    }
-
-    public double getValorTotalRendimento() {
-        return valorTotalRendimento;
-    }
-
-    public double getTarifaMensal() {
-        return tarifaMensal;
-    }
-
-    public void setTarifaMensal(double tarifaMensal) {
-        this.tarifaMensal = tarifaMensal;
-    }
-
-    public double getLimiteChequeEspecial() {
-        return limiteChequeEspecial;
-    }
-
-    public void setLimiteChequeEspecial(double limiteChequeEspecial) {
-        if (getTipo() != ESPECIAL) {
-            throw new RuntimeException("Esse tipo de conta não permite limite de cheque especial");
-        }
-        this.limiteChequeEspecial = limiteChequeEspecial;
-    }
-
-    public void creditarRendimento(double porcentualJuros) {
-        if (getTipo() == INVESTIMENTO || getTipo() == ESPECIAL) {
-            double valorRendimentos = getSaldo() * porcentualJuros / 100;
-            this.valorTotalRendimento += valorRendimentos;
-            depositar(valorRendimentos);
-        } else {
-            throw new RuntimeException("Pode creditar rendimentos nesse tipo de conta");
-        }
-    }
-
     public void sacar(double valorSaque) {
         if (valorSaque <= 0) {
             throw new IllegalArgumentException("Valor do saque deve ser maior que zero");
         }
-        if (getSaldoDisponivel() < valorSaque) {
+        if (getSaldo() < valorSaque) {
             throw new RuntimeException("Saldo insuficiente para saque");
         }
         saldo -= valorSaque;
@@ -122,14 +58,5 @@ public class Conta {
         System.out.printf("Conta: %d%n", getNumero());
         System.out.printf("Titular: %s%n", getTitular().getNome());
         System.out.printf("Saldo: %.2f%n", getSaldo());
-        System.out.printf("Saldo disponível: %.2f%n", getSaldoDisponivel());
-    }
-
-    public void debitarTarifaMensal() {
-        if (getTipo() == ESPECIAL) {
-            sacar(getTarifaMensal());
-        } else {
-            throw new RuntimeException("Não pode debitar tarifa mensal nesse tipo de conta");
-        }
     }
 }
