104af7c757820f9d81d42ac570f2d9f44c9418d6
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-10 20:41:08 -0300

    Projeto para trabalhar com herança

--- src/main/java/jackson/rocha/Principal3.java
+++ src/main/java/jackson/rocha/Principal3.java
@@ -1,15 +1,14 @@
 package jackson.rocha;
 
-import jackson.rocha.banco.Conta;
+import jackson.rocha.banco.ContaEspecial;
 import jackson.rocha.banco.Titular;
 
 public class Principal3 {
     public static void main(String[] args) {
-        Conta conta1 = new Conta();
+        ContaEspecial conta1 = new ContaEspecial();
         conta1.setTitular(new Titular("Jackson", "01422222160"));
         conta1.setAgencia(1234);
         conta1.setNumero(999999);
-        conta1.setTipo(Conta.ESPECIAL);
         conta1.setLimiteChequeEspecial(1000);
         conta1.setTarifaMensal(90);
 
@@ -20,7 +19,7 @@ public class Principal3 {
 
         conta1.sacar(200);
         conta1.imprimirDemonstrativo();
-        
+
         conta1.debitarTarifaMensal();
         conta1.imprimirDemonstrativo();
     }
