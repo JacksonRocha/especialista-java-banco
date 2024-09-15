0871a147f636465c0ea45d5bf92adb34e0a8ac17
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-14 21:32:38 -0300

    Usando atributos nos construtores

--- src/main/java/jackson/rocha/Principal2.java
+++ src/main/java/jackson/rocha/Principal2.java
@@ -5,10 +5,8 @@ import jackson.rocha.banco.Titular;
 
 public class Principal2 {
     public static void main(String[] args) {
-        ContaInvestimento conta1 = new ContaInvestimento();
-        conta1.setTitular(new Titular("Jackson", "01422222160"));
-        conta1.setAgencia(1234);
-        conta1.setNumero(999999);
+        Titular titular = new Titular("Jackson", "01422222160");
+        ContaInvestimento conta1 = new ContaInvestimento(titular, 1234, 999999);
 
         conta1.imprimirDemonstrativo();
 
