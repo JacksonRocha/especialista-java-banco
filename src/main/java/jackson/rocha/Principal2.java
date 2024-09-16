73f67d06148461017c14a9e34b475a211eea5049
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-16 20:01:27 -0300

    Polimorfismo iniciando o projeto

--- src/main/java/jackson/rocha/Principal2.java
+++ src/main/java/jackson/rocha/Principal2.java
@@ -1,19 +1,15 @@
 package jackson.rocha;
 
-import jackson.rocha.banco.ContaInvestimento;
+import jackson.rocha.banco.Conta;
+import jackson.rocha.banco.ContaSalario;
 import jackson.rocha.banco.Titular;
 
 public class Principal2 {
+
     public static void main(String[] args) {
         Titular titular = new Titular("Jackson", "01422222160");
-        ContaInvestimento conta1 = new ContaInvestimento(titular, 1234, 999999);
+        Conta conta = new ContaSalario(titular, 1234, 999999, 800.00);
 
-        conta1.imprimirDemonstrativo();
 
-        conta1.depositar(100);
-        conta1.imprimirDemonstrativo();
-
-        conta1.creditarRendimento(6);
-        conta1.imprimirDemonstrativo();
     }
 }
