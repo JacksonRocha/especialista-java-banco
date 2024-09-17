bcde98ab0cd1a1f89763ec9291f65a6c84d627d2
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-17 13:26:32 -0300

    Usando o downcasting

--- src/main/java/jackson/rocha/banco/CaixaEletronico.java
+++ src/main/java/jackson/rocha/banco/CaixaEletronico.java
@@ -3,8 +3,9 @@ package jackson.rocha.banco;
 public class CaixaEletronico {
 
     public static final double TARIFA_TRANSFERENCIA = 10;
+    public static final double TARIFA_IMPRESSAO_DEMONSTRATIVO = 5;
 
-    public void transferir(ContaEspecial contaOrigen, ContaEspecial contaDestino,
+    public void transferir(Conta contaOrigen, Conta contaDestino,
                            double valorTransferencia) {
         System.out.printf("Transferindo R$%.2f da conta %d/%d para %d/%d%n", valorTransferencia,
                 contaOrigen.getAgencia(), contaOrigen.getNumero(), contaDestino.getAgencia(),
@@ -12,5 +13,19 @@ public class CaixaEletronico {
 
         contaOrigen.sacar(valorTransferencia + TARIFA_TRANSFERENCIA);
         contaDestino.depositar(valorTransferencia);
+    }
+
+    public void imprimirDemonstrativo(Conta conta) {
+
+        ContaInvestimento contaInvestimento = (ContaInvestimento) conta;
+
+        if (contaInvestimento.getValorTotalRendimento() > 0) {
+            System.out.println("Impressão do demonstrativo é gratuita!");
+        } else {
+            System.out.printf("Custo da impressão: R$%.2f%n", TARIFA_IMPRESSAO_DEMONSTRATIVO);
+            conta.sacar(TARIFA_IMPRESSAO_DEMONSTRATIVO);
+        }
+
+        conta.imprimirDemonstrativo();
     }
 }
